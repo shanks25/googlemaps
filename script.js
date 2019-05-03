dc3b72b676a8a870cedc8bf415024679455c7437
shanks25 <kunal.rajput2511@gmail.com>
2019-05-04 00:51:33 +0530

    Most Imp Input Autofill with places address , lat long in other input field added remeber it damn bitch

--- script.js
+++ script.js
@@ -1,8 +1,31 @@
-  var autocomplete;
-  function initialize() {
-  	autocomplete = new google.maps.places.Autocomplete(
-  		(document.getElementById('autocomplete')),
-  		{ types: ['geocode'] });
-  	google.maps.event.addListener(autocomplete, 'place_changed', function() {
-  	});
+ function initializeAutocomplete(){
+    var input = document.getElementById('locality');
+    // var options = {
+    //   types: ['(regions)'],
+    //   componentRestrictions: {country: "IN"}
+    // };
+    var options = {}
+
+    var autocomplete = new google.maps.places.Autocomplete(input, options);
+
+    google.maps.event.addListener(autocomplete, 'place_changed', function() {
+      var place = autocomplete.getPlace();
+      var lat = place.geometry.location.lat();
+      var lng = place.geometry.location.lng();
+      var placeId = place.place_id;
+      // to set city name, using the locality param
+      var componentForm = {
+        locality: 'short_name',
+      };
+      for (var i = 0; i < place.address_components.length; i++) {
+        var addressType = place.address_components[i].types[0];
+        if (componentForm[addressType]) {
+          var val = place.address_components[i][componentForm[addressType]];
+          document.getElementById("city").value = val;
+        }
+      }
+      document.getElementById("latitude").value = lat;
+      document.getElementById("longitude").value = lng;
+      document.getElementById("location_id").value = placeId;
+    });
   }
