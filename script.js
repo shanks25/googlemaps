ed5bce5115fbeccfb9e5934c40184ee86826b2e3
shanks25 <kunal.rajput2511@gmail.com>
2019-05-03 13:30:33 +0530

    places search via input field and get it on maps

--- script.js
+++ script.js
@@ -1,42 +1,94 @@
-// step1
-var map
-function initMap(){
-	var options ={
-		center:{lat:43.654,lng:-79.383},
-		zoom:8,
+ 
+      // This example requires the Places library. Include the libraries=places
+      // parameter when you first load the API. For example:
+      // <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
 
-									 //disableDefaultUI:true ,     disables zoomin zoom out buttom, or fullscreen button ,map/satellite thing ( All attributes here , https://developers.google.com/maps/documentation/javascript/reference/map#MapOptions.disableDefaultUI)
-								 //	draggable:false,      now you can't drag map to other place , it basiaclly sticks to the lat long
-							 //	mapTypeId:'hybrid'  	  the default map type will be satellite, you can change SATELLITE to ROADMAP , or Hybrid which combination of both
- };
-	map = new google.maps.Map(document.getElementById('map'),options);  // to configure the map we pass options object to the class google.maps.Map 
+      function initMap() {
+        var map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: -33.8688, lng: 151.2195},
+          zoom: 13
+        });
+        var card = document.getElementById('pac-card');
+        var input = document.getElementById('pac-input');
+        var types = document.getElementById('type-selector');
+        var strictBounds = document.getElementById('strict-bounds-selector');
 
+        map.controls[google.maps.ControlPosition.TOP_RIGHT].push(card);
 
-}
+        var autocomplete = new google.maps.places.Autocomplete(input);
 
+        // Bind the map's bounds (viewport) property to the autocomplete object,
+        // so that the autocomplete requests use the current map bounds for the
+        // bounds option in the request.
+        autocomplete.bindTo('bounds', map);
 
+        // Set the data fields to return when the user selects a place.
+        autocomplete.setFields(
+            ['address_components', 'geometry', 'icon', 'name']);
 
+        var infowindow = new google.maps.InfoWindow();
+        var infowindowContent = document.getElementById('infowindow-content');
+        infowindow.setContent(infowindowContent);
+        var marker = new google.maps.Marker({
+          map: map,
+          anchorPoint: new google.maps.Point(0, -29)
+        });
 
+        autocomplete.addListener('place_changed', function() {
+          infowindow.close();
+          marker.setVisible(false);
+          var place = autocomplete.getPlace();
+          if (!place.geometry) {
+            // User entered the name of a Place that was not suggested and
+            // pressed the Enter key, or the Place Details request failed.
+            window.alert("No details available for input: '" + place.name + "'");
+            return;
+          }
 
+          // If the place has a geometry, then present it on a map.
+          if (place.geometry.viewport) {
+            map.fitBounds(place.geometry.viewport);
+          } else {
+            map.setCenter(place.geometry.location);
+            map.setZoom(17);  // Why 17? Because it looks good.
+          }
+          marker.setPosition(place.geometry.location);
+          marker.setVisible(true);
 
+          var address = '';
+          if (place.address_components) {
+            address = [
+              (place.address_components[0] && place.address_components[0].short_name || ''),
+              (place.address_components[1] && place.address_components[1].short_name || ''),
+              (place.address_components[2] && place.address_components[2].short_name || '')
+            ].join(' ');
+          }
 
+          infowindowContent.children['place-icon'].src = place.icon;
+          infowindowContent.children['place-name'].textContent = place.name;
+          infowindowContent.children['place-address'].textContent = address;
+          infowindow.open(map, marker);
+        });
 
+        // Sets a listener on a radio button to change the filter type on Places
+        // Autocomplete.
+        function setupClickListener(id, types) {
+          var radioButton = document.getElementById(id);
+          radioButton.addEventListener('click', function() {
+            autocomplete.setTypes(types);
+          });
+        }
 
+        setupClickListener('changetype-all', []);
+        setupClickListener('changetype-address', ['address']);
+        setupClickListener('changetype-establishment', ['establishment']);
+        setupClickListener('changetype-geocode', ['geocode']);
 
-
-
-
-
-
-
-
-  // step 1 we can pass objects directly without creating option object , but above is the clean code
-  // <script>
-  //     var map;
-  //     function initMap() {
-  //       map = new google.maps.Map(document.getElementById('map'), {
-  //         center: {lat: -34.397, lng: 150.644},
-  //         zoom: 8
-  //       });
-  //     }
-  //   </script>
+        document.getElementById('use-strict-bounds')
+            .addEventListener('click', function() {
+              console.log('Checkbox clicked! New state=' + this.checked);
+              autocomplete.setOptions({strictBounds: this.checked});
+            });
+      }
+ 
+ 
