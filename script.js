e00732bfe7aa9d4d55b5addf90d35df070326c9b
shanks25 <kunal.rajput2511@gmail.com>
2019-05-04 12:06:09 +0530

    short code for address autocomplete but no lat long

--- script.js
+++ script.js
@@ -1,31 +1,9 @@
  function initializeAutocomplete(){
     var input = document.getElementById('locality');
-    // var options = {
-    //   types: ['(regions)'],
-    //   componentRestrictions: {country: "IN"}
-    // }; Source- https://stackoverflow.com/questions/3490622/get-latitude-and-longitude-based-on-location-name-with-google-autocomplete-api
+ 
     var options = {}
 
     var autocomplete = new google.maps.places.Autocomplete(input, options);
 
-    google.maps.event.addListener(autocomplete, 'place_changed', function() {
-      var place = autocomplete.getPlace();
-      var lat = place.geometry.location.lat();
-      var lng = place.geometry.location.lng();
-      var placeId = place.place_id;
-      // to set city name, using the locality param
-      var componentForm = {
-        locality: 'short_name',
-      };
-      for (var i = 0; i < place.address_components.length; i++) {
-        var addressType = place.address_components[i].types[0];
-        if (componentForm[addressType]) {
-          var val = place.address_components[i][componentForm[addressType]];
-          document.getElementById("city").value = val;
-        }
-      }
-      document.getElementById("latitude").value = lat;
-      document.getElementById("longitude").value = lng;
-      document.getElementById("location_id").value = placeId;
-    });
+   
   }
